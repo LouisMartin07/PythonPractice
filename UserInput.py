dcbabebfb5dbfc92034c824129722596cd75759a
lmartin <louis.martin11730@gmail.com>
2024-01-02 04:10:21 -0700

    files regarding practice using scrimba to learn python

---
+++ UserInput.py
@@ -0,0 +1,8 @@
+#sample questions to use and intigrate user input
+name= input('What is your name?: ')
+age= input('What is your age?: ')
+print("Hello "+name+" you are " + age+ " years old!") 
+#sample questions to solve a problem with user input
+num1 = input('enter a digit')
+num2 = input('enter a second number')
+answer=float(num1)+float(num2)
