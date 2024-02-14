a104467cd6f5f6cc0b2a0b7e378b61068e590c41
LouisMartin07 <louis.martin11730@gmail.com>
2024-02-14 10:52:20 -0600

    a

---
+++ credit_check.py
@@ -0,0 +1,12 @@
+def credit_check(num):
+    #split the num into individual numbers
+    num_list = list(map(int,str(num)))
+    #multiple every other digit starting at the first one
+    
+    #take the numbers that are doubled, seperate them into indiv numbers and add them
+    #add all the numbers up and if the result % 10 = 0, its valid
+
+
+credit_check(5541808923795240)
+
+
